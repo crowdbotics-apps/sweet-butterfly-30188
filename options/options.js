ac26cc656b2055f640b9d6ec264cf6c120fee27d
Crowdbotics <team@crowdbotics.com>
2021-08-30 01:52:24 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "sweet_butterfly_30188Identifier",
+  url: "https://sweet_butterfly_30188Identifier.botics.co",
+  api: "https://sweet_butterfly_30188Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
